2dfa064bf4cb4591c0f904020ae2f365a6185633
Rogerr Oliva <Roliva75@yahoo.com>
2021-01-22 18:52:05 -0600

    added comments

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ const search = document.getElementById('search');
 console.log(search)
 
 
-/* here I am calling the function getUserData in which it takes string, tbat being the searh.value (user name) */
+/* here I am calling the function getUserData in which it takes string, 
+tbat being the searh.value (user name). This function is called here because we want to be able to display
+the github data and card as soon as the page loads. */
 getUserData('OldManRog');
 
 /* this async function has a promise to return the data from the API.*/
@@ -27,21 +29,29 @@ async function getUserData(username) {
 
     console.log(respData);
 
+    /* When I call getUserData, I get data, but then I also call 
+    displayUserDataOnUserCard, which dislays the data on the card */
     displayUserDataOnUserCard(respData);
 
+    /* When I call getUserData, I get data, but then I also call 
+   getRepoData, which fetches the data from the repos*/
     getRepoData(username); 
 }
 
+/* This function grabs the repo data*/
 async function getRepoData(username){
     const resp =  await fetch(APIURL + username + '/repos');
     const respData = await resp.json();
 
     console.log(respData)
 
+    /* similar as to when I cal getUserData, when I cal getRepoData, I also
+    want to call displayRepoDataOnUserCard, which will display the repos on the card */
     displayRepoDataOnUserCard(respData);
 }
 
 
+/* This function is used to display the github profile details on the card*/
 function displayUserDataOnUserCard(user) {
  
 
@@ -49,9 +59,15 @@ function displayUserDataOnUserCard(user) {
     /*const card = document.createElement('div')
     card.classList.add('card')*/
 
+    /* this is not an array, so I can create a const which will equal to the HTML 
+    I want to display dynamically.*/
+
+    /* this funtion takes a string that could be called anything. 
+    For it to makes sense we called it user. for each section that is dynamic
+    I have to grab it from the API object as seen below as user._____ */
     const cardHTML = `
     <div class="card">
-    <div>
+    <div>                    
     <img class="avatar" src="${user.avatar_url}" alt="${user.name}">
     </div>
     <div class="user-info">
@@ -73,17 +89,27 @@ function displayUserDataOnUserCard(user) {
     `
     console.log(cardHTML);
 
+    /* I created a const that represents the main. on that main, the html inside the
+    main, I want it to be the html from the cardHTML */
     main.innerHTML = cardHTML;
 }
 
 
-
+/* this function takes a string, and for it to make sense we calle it repo */
 function displayRepoDataOnUserCard(repos) {
+    /* we create const that represents the div which will hold the repos
+    we need to include it down here because the element is created only after displayUserDataOnCard is called */
     const reposEl = document.getElementById('repos')
     console.log(reposEl)
 
     console.log(repos)
 
+    /* we named the array within the object, repos, and for each repo in repos,
+     we want to create an a tag in order to include a link. For each 
+     a tag we also want to add the class repo. we do this by using a const called repoEl. 
+     We then specify what is the HTML we want inside the repoEl. 
+     Finally we can then append the repoEL which is the a tag that was created for each repo in the repos array,
+     and append each one to the reposEl, which is the parent div */
     repos.forEach(repo => {
         const repoEl = document.createElement('a')
         repoEl.classList.add('repo')
@@ -99,12 +125,18 @@ function displayRepoDataOnUserCard(repos) {
     })
    
 }    
-
+/* this is an event listner for the form. When I submit,
+it prevents the default behavior. I then assing username to the search.valu. 
+search.value will be the input that is subumitte. search comes from the variable that is
+assign to the search element.*/
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const username = search.value;
     
+    /* if there is a username, meaning an input value,
+    the call getUserData with the username as the paramater. Rmb username is the input value
+    once It cals getUserData, the next step is to clear the input*/
     if(username) {
         getUserData(username);
 
